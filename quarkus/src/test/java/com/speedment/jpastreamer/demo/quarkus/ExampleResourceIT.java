1d4ae3a0e942236c649495579145dcb2d55990e8
Julia Gustafsson <jgustafsson6@gatech.edu>
2023-06-02 14:35:37 +0200

    Add utilities for Quarkus demo

---
+++ quarkus/src/test/java/com/speedment/jpastreamer/demo/quarkus/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package com.speedment.jpastreamer.demo.quarkus;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
