21c379f24f8b6c6af0da0fdc21d9a11ac7bd0e9a
Julia Gustafsson <jgustafsson6@gatech.edu>
2023-08-30 15:31:49 +0200

    Add mapMulti()-demo

--- hibernate/src/main/java/com/speedment/jpastreamer/demo/hibernate/MapMultiDemo.java
+++ hibernate/src/main/java/com/speedment/jpastreamer/demo/hibernate/MapMultiDemo.java
@@ -1,2 +1,27 @@
-package com.speedment.jpastreamer.demo.hibernate;public class MapMultiDemo {
+package com.speedment.jpastreamer.demo.hibernate;
+
+import com.speedment.jpastreamer.application.JPAStreamer;
+import com.speedment.jpastreamer.demo.hibernate.model.Film;
+import com.speedment.jpastreamer.demo.hibernate.model.Film$;
+import com.speedment.jpastreamer.streamconfiguration.StreamConfiguration;
+
+public class MapMultiDemo {
+
+    public static void main(String[] args) {
+
+        JPAStreamer jpaStreamer = JPAStreamer.of("sakila");
+
+        jpaStreamer.stream(StreamConfiguration.of(Film.class).joining(Film$.actors))
+                .filter(Film$.length.greaterThan(120))
+                .mapMulti((f, mapper) -> {
+                    f.getActors().stream()
+                            .filter(a -> a.getFirstName().startsWith("A"))
+                            .forEach(mapper);
+                })
+                .forEach(System.out::println);
+
+        jpaStreamer.close();
+
+    }
+    
 }
